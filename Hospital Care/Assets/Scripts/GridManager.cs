091f837be9a4c27029b7fdbd4710c0598282b895
Death1942 <Whitewolf1942@live.com.au>
2018-01-29 10:07:26 +1100

    Pushed project into repository.

---
+++ Hospital Care/Assets/Scripts/GridManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField]
+    public float SizeOfGrid = 1.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+    }
+
+    public Vector3 GetNearestGridPoint(Vector3 rayPosition)
+    {
+        rayPosition -= transform.position;
+
+        int xCount = Mathf.RoundToInt(rayPosition.x / SizeOfGrid);
+        int yCount = Mathf.RoundToInt(rayPosition.y / SizeOfGrid);
+        int zCount = Mathf.RoundToInt(rayPosition.z / SizeOfGrid);
+
+        Vector3 result = new Vector3((float)xCount * SizeOfGrid, (float)yCount * SizeOfGrid, (float)zCount * SizeOfGrid);
+
+        result += transform.position;
+
+        return result;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        for(float x = this.transform.position.x; x < this.transform.position.x + 40; x += SizeOfGrid)
+        {
+            for(float z = this.transform.position.z; z < this.transform.position.z + 40; z += SizeOfGrid)
+            {
+                var point = GetNearestGridPoint(new Vector3(x, 0.0f, z));
+                Gizmos.DrawSphere(point, 0.1f);
+            }
+        }
+    }
+}
---
+++ Hospital Care/Assets/Scripts/ObjectPlacer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPlacer : MonoBehaviour
+{
+    private GridManager gridObject;
+
+	// Use this for initialization
+	void Start ()
+    {
+        gridObject = FindObjectOfType<GridManager>();
+        if(gridObject == null)
+        {
+            Debug.LogError("Could not find grid manager, please ");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit rayHit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            //if we clicked near something
+            if(Physics.Raycast(ray, out rayHit))
+            {
+                //Place on the grid
+                PlaceObjectNear(rayHit.point);
+            }
+        }
+	}
+
+    private void PlaceObjectNear(Vector3 rayPoint)
+    {
+        var gridPosition = gridObject.GetNearestGridPoint(rayPoint);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = gridPosition;
+    }
+}
